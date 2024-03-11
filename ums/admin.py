024e02a09cfc5c6b1297f314d9998d839985a324
Priyanshu Rana <priyanshurana1234@gmail.com>
2024-03-11 23:56:58 +0530

    django-admin-initial-setup

--- ums/admin.py
+++ ums/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, Teacher, Deparment
+
 
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = {"id", "name", "age", "email", "address"}
